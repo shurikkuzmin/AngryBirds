65b82b7b9a21d051add2eaf4699ee19ffd4d1756
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-02-18 10:13:10 -0500

    Added ball example.

--- example.py
+++ example.py
@@ -16,15 +16,22 @@ clock = pygame.time.Clock()
 fps = 60
 
 space = pymunk.Space()
-space.gravity = 0.0, -10.0
+space.gravity = 0.0, -200.0
+space.damping = 0.9
 
 ball = pymunk.Body()
 ball.position = (100.0,300.0)
+ball.velocity = 50.0, 20.0
 ball_shape = pymunk.Circle(ball, 30.0)
 ball_shape.density = 1.0
+ball_shape.elasticity = 1.0
+ball_shape.friction = 1.0
+
 
 earth = pymunk.Body(body_type = pymunk.Body.STATIC)
 earth_shape = pymunk.Segment(earth, (0.0, 100.0), (600.0, 100.0), 5.0)
+earth_shape.elasticity = 0.7
+earth_shape.friction = 1.0
 
 space.add(ball, ball_shape, earth, earth_shape)
 
@@ -35,7 +42,9 @@ while isRunning:
         if event.type == pygame.QUIT:
             isRunning = False
     x, y = ball.position
-    pygame.draw.circle(screen, (255, 0, 0), (int(x), int(y)), 30)
+    pygame.draw.circle(screen, (255, 0, 0), (int(x), 400 - int(y)), 30)
+    
+    pygame.draw.line(screen, (0,255,0), (0, 300), (600, 300),4)
     
     clock.tick(fps)
     space.step(1.0 / fps)
