a83ed3d216dd5faf7ebad835f4569e325078f49e
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-05-06 09:47:01 -0400

    Temporary commit.

--- angry_birds.py
+++ angry_birds.py
@@ -117,7 +117,7 @@ class Bird():
         image1 = sprites.subsurface(517,913,65,55)
         image2 = sprites.subsurface(585,913,65,55)
         image3 = sprites.subsurface(651,913,65,55)
-        image4 = sprites.subsurface(536,870,65,55)
+        image4 = sprites.subsurface(536,842,65,55)
         self.images = [image1, image2, image3, image4]
         #self.image = sprites.subsurface(513,913,70,55)
         self.image = self.images[0]
@@ -238,11 +238,18 @@ class Bird():
         x, y = self.bird_body.position                  
         self.rect.center = convert_to_pygame(x, y)
 
-def test(arb, space, data):
-    print("Privet!")
+def collision_bird_wood(arb, space, data):
+    print("Wood!")
     return True
+
+def collision_bird_pig(arb, space, data):
+    print("Pig!")
+    return True    
+
 handler_bird_wood = space.add_collision_handler(1, 3)
-handler_bird_wood.begin = test
+handler_bird_wood.begin = collision_bird_wood
+
+handle_bird_pig = space.add_collision_handler()
 
 # Bird is created in physics coordinates
 pole1 = Pole("Vertical", 740.0, 185.0)
